996561399006493e160c1fd6b0b139c35dfc8364
Ricardo Maes <ricmzn@gmail.com>
2016-10-05 20:21:54 -0300

    Otimizado processo de upload, removido JQuery

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 let express = require("express");
 let multer = require("multer");
+let fs = require("fs");
 
 const listenPort = 8080;
-const upload = multer({storage: multer.memoryStorage()});
+const upload = multer({dest: "uploads/"});
 const app = express();
 
 app.use(express.static('public'));
@@ -11,6 +12,7 @@ app.post("/upload", upload.single("image"), function(req, res) {
     console.log(req.file);
     if (req.file) { 
         res.sendStatus(200);
+        fs.unlinkSync(req.file.path);
     }
     else {
         res.sendStatus(400);
--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,11 @@
-require("script!jquery");
 require("materialize-css/dist/css/materialize.css");
 
-$(document).ready(function() {
-    $("#loader").addClass("hidden");
-    $("#content").removeClass("hidden");
+setLoaderVisible(false);
+
+document.getElementById("submit").addEventListener("click", function() {
+    setLoaderVisible(true);
+});
+
+document.getElementById("form").addEventListener("submit", function() {
+    setLoaderVisible(false);
 });
