f390cb17755cba0e46dca23e2418c4cf5fe208a1
Ricardo Maes <ricmzn@gmail.com>
2016-10-06 16:48:42 -0300

    Adicionada prévia da imagem

--- public/index.js
+++ public/index.js
@@ -11,6 +11,17 @@ function setLoaderVisible(state) {
     }
 }
 
+function preview(event) {
+    var img = $("#preview").get(0);
+    var file = event.files[0];
+    var url = URL.createObjectURL(file);
+    img.src = url;
+}
+
+$("#image").on("change", function() {
+    preview($(this).get(0));
+});
+
 $("#submit").on("click", function() {
     setLoaderVisible(true);
 });
