ff398d8a31d9818aa6dc1a3fe31b05f390ac6f8d
Ricardo Maes <ricmzn@gmail.com>
2016-10-07 16:27:20 -0300

    Novo layout do formulário

--- public/index.js
+++ public/index.js
@@ -19,18 +19,6 @@ function getSelectedImage() {
     return $("#image").get(0).files[0];
 }
 
-function getQueryParam(key) {
-    var query = window.location.search;
-    if (query) {
-        var regex = new RegExp(key + "=([^&]*)");
-        var result = regex.exec(query);
-        if (result) {
-            return result[1];
-        }
-    }
-    return undefined;
-}
-
 function validateForm() {
     var image = getSelectedImage();
     setSubmitEnabled(image != undefined);
@@ -47,21 +35,3 @@ $(window).on("pagehide", function() {
 });
 
 validateForm();
-
-switch (parseInt(getQueryParam("status"))) {
-    // Sucesso
-    case 0:
-        $("#message").text("Imagem enviada com sucesso");
-        break;
-    // Falha
-    case 1:
-        $("#message").text("Erro no envio da imagem");
-        break;
-    case 2:
-        $("#message").text("Senha de envio incorreta");
-        break;
-    // Nada
-    default:
-        $("#message").text("");
-        break;
-}
